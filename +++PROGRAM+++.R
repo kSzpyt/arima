aae78390614d5ae71cfa139996a4a7d746362272
kSzpyt <karolo.szpyt@gmail.com>
2020-03-01 23:42:26 +0100

    ...

--- +++PROGRAM+++.R
+++ +++PROGRAM+++.R
@@ -23,43 +23,68 @@ s2 <- seq(8, dim(data2)[2], by = x)
 #liczba zaprognozowanych dni
 n <- 14
 #te dwie linijki kodu poniżej sprawią że uruchomi się tylko jeden, pierwszy bankomat
-#wykomentarzować żeby odpalić wszystkie bankomaty
-s1 <- s1[1]
-s2 <- s2[1]
-#data początku i końca modelowania- n dni prognozy będzie po dacie "end"
-start <- "2018-01-01"
-end <- "2019-11-30"
+# wykomentarzować żeby odpalić wszystkie bankomaty
+s1 <- s1[-(1:8)]
+s2 <- s2[-(1:8)]
+# data początku i końca modelowania- n dni prognozy będzie po dacie "end"
+start <- "2019-05-04"
+end <- "2019-11-03"
 ###
 
 #wszystko poniżej uruchomić
-# i <- 1
-# for (x in s1)
-# {
-#   a <- selfarima.means(data2, start.date = start, end.date = end, nr = x, type = "simple", n = n, seas = TRUE)
-#   write.means(a, data2, typ = "simple", i = i, data.nas = data3, nr = x)
-#   
-#   b <- selfarima.means(data2, start.date = start, end.date = end, nr = x, type = "xreg", xr = as.matrix(ds), n = n, seas = TRUE)
-#   write.means(b, data2, typ = "xreg_ds", i = i, data.nas = data3, nr = x)
-#   
-#   c <- selfarima.means(data2, start.date = start, end.date = end, nr = x, type = "xreg", xr = as.matrix(dns), n = n, seas = FALSE)
-#   write.means(c, data2, typ = "xreg_dns", i = i, data.nas = data3, nr = x)
-#   
-#   i <- i + 1
-# }
-
+x <- 0
 i <- 1
-for (x in s2) 
+for (x in s1)
 {
-  selfarima2(data2, start.date = start, end.date = end, nr = x, result.save = TRUE, i = i, type = "simple", n = n, seas = TRUE, log = TRUE , typ = "simple_log", data.nas = data3)
-  selfarima2(data2, start.date = start, end.date = end, nr = x, result.save = TRUE, i = i, type = "xreg", xr = as.matrix(ds), n = n, seas = TRUE, log = TRUE , typ = "xreg_ds_log", data.nas = data3)
-  selfarima2(data2, start.date = start, end.date = end, nr = x, result.save = TRUE, i = i, type = "xreg", xr = as.matrix(dns), n = n, seas = FALSE, log = TRUE , typ = "xreg_dns_log", data.nas = data3)
+  a <- selfarima.means(data2, start.date = start, end.date = end, nr = x, type = "simple", n = n, seas = TRUE)
+  write.means(a, data2, typ = "simple", i = i, data.nas = data3, nr = x)
+  x <- x+1
+
+  b <- selfarima.means(data2, start.date = start, end.date = end, nr = x, type = "xreg", xr = as.matrix(ds), n = n, seas = TRUE)
+  write.means(b, data2, typ = "xreg_ds", i = i, data.nas = data3, nr = x)
+  x <- x+1
+
+  c <- selfarima.means(data2, start.date = start, end.date = end, nr = x, type = "xreg", xr = as.matrix(dns), n = n, seas = FALSE)
+  write.means(c, data2, typ = "xreg_dns", i = i, data.nas = data3, nr = x)
+  x <- x+1
   
-  selfarima2(data2, start.date = start, end.date = end, nr = x, result.save = TRUE, i = i, type = "simple", n = n, seas = TRUE, log = FALSE , typ = "simple", data.nas = data3)
-  selfarima2(data2, start.date = start, end.date = end, nr = x, result.save = TRUE, i = i, type = "xreg", xr = as.matrix(ds), n = n, seas = TRUE, log = FALSE , typ = "xreg_ds", data.nas = data3)
-  selfarima2(data2, start.date = start, end.date = end, nr = x, result.save = TRUE, i = i, type = "xreg", xr = as.matrix(dns), n = n, seas = FALSE, log = FALSE , typ = "xreg_dns", data.nas = data3)
+  a.log <- selfarima.means(data2, start.date = start, end.date = end, nr = x, type = "simple", n = n, seas = TRUE, log = TRUE)
+  write.means(a.log, data2, typ = "simple_log", i = i, data.nas = data3, nr = x)
+  x <- x+1
+
+  b.log <- selfarima.means(data2, start.date = start, end.date = end, nr = x, type = "xreg", xr = as.matrix(ds), n = n, seas = TRUE, log = TRUE)
+  write.means(b.log, data2, typ = "xreg_ds_log", i = i, data.nas = data3, nr = x)
+  x <- x+1
+
+  c.log <- selfarima.means(data2, start.date = start, end.date = end, nr = x, type = "xreg", xr = as.matrix(dns), n = n, seas = FALSE, log = TRUE)
+  write.means(c.log, data2, typ = "xreg_dns_log", i = i, data.nas = data3, nr = x)
+  x <- x+1
+  
   
   i <- i + 1
 }
-#547
 
+beep()
+
+y <- 0
+j <- 1
+for (x in s2)
+{
+  selfarima2(data2, start.date = start, end.date = end, nr = x, result.save = TRUE, i = j, type = "simple", n = n, seas = TRUE, log = TRUE , typ = "simple_log", data.nas = data3)
+  y <- y+1
+  selfarima2(data2, start.date = start, end.date = end, nr = x, result.save = TRUE, i = j, type = "xreg", xr = as.matrix(ds), n = n, seas = TRUE, log = TRUE , typ = "xreg_ds_log", data.nas = data3)
+  y <- y+1
+  selfarima2(data2, start.date = start, end.date = end, nr = x, result.save = TRUE, i = j, type = "xreg", xr = as.matrix(dns), n = n, seas = FALSE, log = TRUE , typ = "xreg_dns_log", data.nas = data3)
+  y <- y+1
+
+  selfarima2(data2, start.date = start, end.date = end, nr = x, result.save = TRUE, i = j, type = "simple", n = n, seas = TRUE, log = FALSE , typ = "simple", data.nas = data3)
+  y <- y+1
+  selfarima2(data2, start.date = start, end.date = end, nr = x, result.save = TRUE, i = j, type = "xreg", xr = as.matrix(ds), n = n, seas = TRUE, log = FALSE , typ = "xreg_ds", data.nas = data3)
+  y <- y+1
+  selfarima2(data2, start.date = start, end.date = end, nr = x, result.save = TRUE, i = j, type = "xreg", xr = as.matrix(dns), n = n, seas = FALSE, log = FALSE , typ = "xreg_dns", data.nas = data3)
+  y <- y+1
+
+  j <- j + 1
+}
+#547
 #zerowe dni specjalne
